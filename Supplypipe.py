2188571b61f18974dc270dcbd4977154607be067
Daniel Boloc <daniel.boloc@stalicla.com>
2021-02-26 00:01:37 +0100

    changed into main dir

--- Supplypipe.py
+++ Supplypipe.py
@@ -1,36 +1,44 @@
-from config import get_configuration
+from supplypipe.config import get_configuration
+from supplypipe.utils import list2str
+from supplypipe.downloader import download
 import yfinance as yf
 import click
 import plotly.graph_objects as go
 
 @click.command()
 @click.option("--only-stock",
-	          help="To only look at the 'stock' section in config")
+              is_flag=True,
+              help="To only look at the 'stock' section in config")
 @click.option("--on-demand",
-	          help="You can put the ticker symbol, e.g MSFT or multiple MSFT, AAPL")
+              help="You can put the ticker symbol, e.g MSFT or multiple MSFT, AAPL")
 def main(only_stock, on_demand):
-	config = get_configuration()
-	# tickers = yf.Tickers(config["SECTORS"]["technology"])
-	# print(tickers.tickers.QQQ.history(period="1mo"))
-	# print(tickers.tickers.QQQ.major_holders)
-	for sector in config["SECTORS"]:
-		print(f"Downloading sector: {sector}")
-		data = yf.download(config["SECTORS"][sector],
-						   group_by='ticker',
-						   auto_adjust=True,
-			               start='2021-01-01',
-			               end='2021-02-18')
+    config = get_configuration()
+    # tickers = yf.Tickers(config["SECTORS"]["technology"])
+    # print(tickers.tickers.QQQ.history(period="1mo"))
+    # print(tickers.tickers.QQQ.major_holders)
+    if only_stock:
+        securities2download = config["STOCKS"]["securities"] # str
+    elif on_demand:
+        securities2download = on_demand # str
+    else:
+        securities2download = list2str(list(config["SECTORS"].values())) # str
 
-		for security in config["SECTORS"][sector].split(", "):
-			df = data[security]
-			fig = go.Figure(data=[go.Candlestick(x=df.loc['2021-01-01':'2021-02-18'],
-			                open=df['Open'],
-			                high=df['High'],
-			                low=df['Low'],
-			                close=df['Close'])])
+    print(securities2download)
+    data = download(securities2download,
+                    start='2021-01-01',
+                    end='2021-02-18')
+    print(data)
+    breakpoint()
 
-			fig.show()
-			breakpoint()
+        # for security in config["SECTORS"][sector].split(", "):
+        #     df = data[security]
+        #     fig = go.Figure(data=[go.Candlestick(x=df.loc['2021-01-01':'2021-02-18'],
+        #                     open=df['Open'],
+        #                     high=df['High'],
+        #                     low=df['Low'],
+        #                     close=df['Close'])])
+
+        #     fig.show()
 
 if __name__ == '__main__':
-	main()
+    main()
