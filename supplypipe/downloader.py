aab2faacf0a7dbea1307cd7b4dd42f6ce7c1df03
Daniel Boloc <danielboloc@gmail.com>
2021-02-28 22:49:13 +0100

    changed download periods for different timeframes

--- supplypipe/downloader.py
+++ supplypipe/downloader.py
@@ -1,10 +1,36 @@
 import yfinance as yf
+from supplypipe.utils import calculate_download_days
 
-def download(list_of_securities, **kwargs):
+def download(list_of_securities, intervals, **kwargs):
 
-	data = yf.download(list_of_securities,
+	intervals = intervals.replace(',',' ').split()
+	start_date4H, end_date4H = calculate_download_days(59)
+	start_date1D, end_date1D = calculate_download_days(60)
+	start_date1W, end_date1W = calculate_download_days(240)
+
+	h = yf.download(list_of_securities,
 					   group_by='ticker',
 					   auto_adjust=True,
-		               **kwargs)
+					   interval=intervals[0],
+					   period='max',
+		               #**kwargs)
+		               start=start_date4H,
+		               end=end_date4H)
 
-	return data
+	D = yf.download(list_of_securities,
+					   group_by='ticker',
+					   auto_adjust=True,
+					   interval=intervals[1],
+					   period='max',
+		               start=start_date1D,
+		               end=end_date1D)
+
+	W = yf.download(list_of_securities,
+					   group_by='ticker',
+					   auto_adjust=True,
+					   interval=intervals[2],
+					   period='max',
+		               start=start_date1W,
+		               end=end_date1W)
+
+	return h, D, W
