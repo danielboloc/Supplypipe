515653fea8bec382ac7b97e24e9212fb86842066
Daniel Boloc <daniel.boloc@stalicla.com>
2021-03-02 23:54:56 +0100

    updated days for download

--- supplypipe/downloader.py
+++ supplypipe/downloader.py
@@ -4,7 +4,7 @@ from supplypipe.utils import calculate_download_days
 def download(list_of_securities, intervals, **kwargs):
 
 	intervals = intervals.replace(',',' ').split()
-	start_date4H, end_date4H = calculate_download_days(59)
+	start_date4H, end_date4H = calculate_download_days(29)
 	start_date1D, end_date1D = calculate_download_days(60)
 	start_date1W, end_date1W = calculate_download_days(240)
 
@@ -16,6 +16,7 @@ def download(list_of_securities, intervals, **kwargs):
 		               #**kwargs)
 		               start=start_date4H,
 		               end=end_date4H)
+		               #prepost = True)
 
 	D = yf.download(list_of_securities,
 					   group_by='ticker',
@@ -23,7 +24,8 @@ def download(list_of_securities, intervals, **kwargs):
 					   interval=intervals[1],
 					   period='max',
 		               start=start_date1D,
-		               end=end_date1D)
+		               end=end_date1D,
+		               prepost = True)
 
 	W = yf.download(list_of_securities,
 					   group_by='ticker',
@@ -31,6 +33,7 @@ def download(list_of_securities, intervals, **kwargs):
 					   interval=intervals[2],
 					   period='max',
 		               start=start_date1W,
-		               end=end_date1W)
+		               end=end_date1W,
+		               prepost = True)
 
 	return h, D, W
