53cddaa172220bc139e3ea87ae7210b283fcf500
Daniel Boloc <danielboloc@gmail.com>
2022-07-22 14:44:40 +0200

    style(plots): add print statements across execution

--- supplypipe/plots.py
+++ supplypipe/plots.py
@@ -23,6 +23,7 @@ def mtf(security,path,hour4, day, week, exp3, exp5, exp7, exp15, exp15_w, exp21)
 
     # initialize settings, where if price is up the color is green, down is red,
     # and to include the volume
+    print("\nSetting up plots\n")
     mc = mpf.make_marketcolors(up='g',down='r',volume='in')
     s  = mpf.make_mpf_style(marketcolors=mc)
 
@@ -33,6 +34,7 @@ def mtf(security,path,hour4, day, week, exp3, exp5, exp7, exp15, exp15_w, exp21)
     ax3 = fig.add_subplot(4,1,3)
     ax4 = fig.add_subplot(4,1,4)
  
+    print("Drawing EMAs")
     # create the EMA crossover lines
     seven_twenty_one = [ mpf.make_addplot(exp7,color='red',ax=ax1),
                          mpf.make_addplot(exp21,color='green',ax=ax1) ]
@@ -41,6 +43,7 @@ def mtf(security,path,hour4, day, week, exp3, exp5, exp7, exp15, exp15_w, exp21)
     five_fifteen = [ mpf.make_addplot(exp5,color='red',ax=ax3),
                      mpf.make_addplot(exp15_w,color='green',ax=ax3) ]
  
+    print("Plotting candlesticks")
     # plot the data in candlesticks at various timeframes, and add EMA lines
     mpf.plot(hour4, type='candle', ax=ax1, axtitle='4H',
              addplot=seven_twenty_one, xrotation=0)
@@ -48,5 +51,7 @@ def mtf(security,path,hour4, day, week, exp3, exp5, exp7, exp15, exp15_w, exp21)
              addplot=three_fifteen, xrotation=0)
     mpf.plot(week, type='candle', ax=ax3,axtitle='1W', addplot=five_fifteen,
              xrotation=0)
+    print("Saving plot")
     plt.suptitle(f"{security.upper()}")
     plt.savefig(f'{path}/{security.upper()}.pdf',dpi=96)
+    print("DONE\n")
